d1ab5c646b54bda79dce1b793fbb9896d63319a6
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:03:31 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "mssql_outbound_firewall_rules_id" {
+  description = "Map of id values across all mssql_outbound_firewall_rules, keyed the same as var.mssql_outbound_firewall_rules"
+  value       = { for k, v in azurerm_mssql_outbound_firewall_rule.mssql_outbound_firewall_rules : k => v.id }
+}
 output "mssql_outbound_firewall_rules_name" {
   description = "Map of name values across all mssql_outbound_firewall_rules, keyed the same as var.mssql_outbound_firewall_rules"
   value       = { for k, v in azurerm_mssql_outbound_firewall_rule.mssql_outbound_firewall_rules : k => v.name }
