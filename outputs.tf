0e10e8783066286f5015a7cb1682c2ebc42f5675
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:05 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,9 @@
-output "mssql_outbound_firewall_rules" {
-  description = "All mssql_outbound_firewall_rule resources"
-  value       = azurerm_mssql_outbound_firewall_rule.mssql_outbound_firewall_rules
-}
 output "mssql_outbound_firewall_rules_name" {
-  description = "List of name values across all mssql_outbound_firewall_rules"
-  value       = [for k, v in azurerm_mssql_outbound_firewall_rule.mssql_outbound_firewall_rules : v.name]
+  description = "Map of name values across all mssql_outbound_firewall_rules, keyed the same as var.mssql_outbound_firewall_rules"
+  value       = { for k, v in azurerm_mssql_outbound_firewall_rule.mssql_outbound_firewall_rules : k => v.name }
 }
 output "mssql_outbound_firewall_rules_server_id" {
-  description = "List of server_id values across all mssql_outbound_firewall_rules"
-  value       = [for k, v in azurerm_mssql_outbound_firewall_rule.mssql_outbound_firewall_rules : v.server_id]
+  description = "Map of server_id values across all mssql_outbound_firewall_rules, keyed the same as var.mssql_outbound_firewall_rules"
+  value       = { for k, v in azurerm_mssql_outbound_firewall_rule.mssql_outbound_firewall_rules : k => v.server_id }
 }
 
